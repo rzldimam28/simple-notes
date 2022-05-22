0f9a643569997ca0d1cc218865901cd8272cc202
Imam Rizaldi <71388723+rzldimam28@users.noreply.github.com>
2022-05-22 11:30:20 +0800

    Update database.go

--- app/database.go
+++ app/database.go
@@ -7,7 +7,7 @@ import (
 )
 
 func NewDB() *sql.DB {
-	db, err := sql.Open("mysql", "root:leesrcyng__@tcp(localhost:3306)/simple_notes?parseTime=true")
+	db, err := sql.Open("mysql", "root:__@tcp(localhost:3306)/simple_notes?parseTime=true")
 	helper.PanicIfError(err)
 	return db
 }
